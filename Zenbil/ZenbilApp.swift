a0a249c5eb42a4d2439f32a06f82282db9e1ed13
Berhan Witte <berhan.witte@gmail.com>
2024-05-09 18:19:45 +0200

    Initial Commit

---
+++ Zenbil/ZenbilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenbilApp.swift
+//  Zenbil
+//
+//  Created by Berhan Witte on 09.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ZenbilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
